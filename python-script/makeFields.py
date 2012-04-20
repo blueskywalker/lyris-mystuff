5ec5cf4bd2b665a2172d4ca31ed4fc14f942bac9
Kyung Seop Kim <jerrykskim@gmail.com>
2012-04-19 18:18:32 -0700

    add number type converter

--- python-script/makeFields.py
+++ python-script/makeFields.py
@@ -1,33 +1,45 @@
+#!/usr/bin/env python
+# $Author$
+# $Date$
 
 #import easy to use xml parser called minidom:
 from xml.dom.minidom import parse
 import sys
 
-if(len(sys.argv)<3):
-    print "It needs 'contact_format.xml' 'schema.xml'"
-    exit()
-    
-fileName=sys.argv[1]
-dom = parse(fileName)
-regions = dom.getElementsByTagName('Region')
 
-fields=dict()
-for r in regions:
-    fields[r.getAttribute("field")]=r.getAttribute("type")
+def detectNumericType(type):
+    if type.lower() == "number":
+        return "long"
+        
+def main(args):
+    if(len(args)<3):
+        print "It needs 'contact_format.xml' 'schema.xml'"
+        exit()
+        
+    fileName=args[1]
+    dom = parse(fileName)
+    regions = dom.getElementsByTagName('Region')
     
-dom = parse(sys.argv[2])
-exists = dom.getElementsByTagName('field')
-existDict = dict()
-
-for  r in exists:
-    existDict[r.getAttribute('name')]=r.getAttribute('type')
-
-for fn in existDict.viewkeys():
-    if (fields.has_key(fn)):
-        continue
+    fields=dict()
+    for r in regions:
+        fields[r.getAttribute("field")]=r.getAttribute("type")
+        
+    dom = parse(args[2])
+    exists = dom.getElementsByTagName('field')
+    existDict = dict()
     
-    fields[fn]=existDict[fn]
-
-for f in sorted(fields.iterkeys()):
-    print '<field name="%s" type="%s" indexed="true" store="true" />' %( f,  fields[f])
+    for  r in exists:
+        existDict[r.getAttribute('name')]=r.getAttribute('type')
     
+    for fn in existDict.viewkeys():
+        if (fields.has_key(fn)):
+            continue
+        
+        fields[fn]=existDict[fn]
+    
+    for f in sorted(fields.iterkeys()):
+        print '<field name="%s" type="%s" indexed="true" store="true" />' %( f,  fields[f])
+
+    
+if __name__ == '__main__':
+    main(sys.argv)
