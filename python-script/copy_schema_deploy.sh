9c33ab678902bdde97cb18f8115c72c62861ee5a
Jerry Kim <jerrykskim@gmail.com>
2012-05-25 16:13:59 -0700

    lowercase indexing

--- python-script/copy_schema_deploy.sh
+++ python-script/copy_schema_deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-scp schema.xml root@10.3.202.149:/root/qa-backend/schema
+PLATFORM=DEV
+
+if [ $# -ge 1 -a $1 == "qa" ]
+then
+PLATFORM=QA
+fi
+
+echo scp schema.xml root@10.3.202.149:/root/qa-backend/schema/${PLATFORM}
+scp schema.xml root@10.3.202.149:/root/qa-backend/schema/${PLATFORM}
