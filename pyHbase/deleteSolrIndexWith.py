bcce93f6038601b91da830b492b88fe608c09e73
Jerry Kim <jerrykskim@gmail.com>
2012-06-20 10:55:39 -0700

    add TableDrop.py

---
+++ pyHbase/deleteSolrIndexWith.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import solr
+import commands
+import ConfigParser
+
+def getConfiguration(filename):
+    config = ConfigParser.RawConfigParser()
+    config.read(filename)
+    
+    global solrHost
+    solrHost = config.get('hosts','solr')
+    
+def connectSolrServer(orgId,subOrgId):
+    global solrServer
+    coreNo = commands.getoutput("./getCore.sh %s %s"%(orgId,subOrgId))
+    hostUrl = "http://%s:8983/solr/%s"%(solrHost,coreNo)
+    solrServer = solr.SolrConnection(hostUrl)
+    
+def main(args):
+    
+    if(len(args) < 3):
+        print "%s orgid subOrgId"%(args[0])
+        sys.exit(-1)
+    
+    getConfiguration('host.properties')
+    
+    connectSolrServer(args[1],args[2])
+    
+    solrServer.delete_query("orgId:%s AND subOrgId:%s"%(args[1],args[2]))
+    
+    solrServer.commit()
+    solrServer.close()
+    
+    
+if __name__ == '__main__':
+    main(sys.argv)
