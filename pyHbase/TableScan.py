4494c48f0021d7cadcab56294c68db6c510c6351
blueskywalker <jerrykskim@gmail.com>
2012-12-05 18:11:03 -0800

    use properties file

--- pyHbase/TableScan.py
+++ pyHbase/TableScan.py
@@ -7,14 +7,17 @@ from thrift.transport.TSocket import TSocket
 from thrift.transport.TTransport import TBufferedTransport
 from thrift.protocol import TBinaryProtocol
 from hbase import Hbase 
+import ConfigParser
 
  
-hbaseHost = '10.3.203.146'
-#hbaseHost = '10.3.9.18'
-transport = TBufferedTransport(TSocket(hbaseHost, 9090))
-transport.open()
-protocol = TBinaryProtocol.TBinaryProtocol(transport)
-client = Hbase.Client(protocol)
+hbaseHost=""
+
+def getConfiguration(filename):
+    config = ConfigParser.RawConfigParser()
+    config.read(filename)
+    global hbaseHost
+    
+    hbaseHost = config.get('hosts','hbase')
 
 def getColumnInfo(table_name):
     
@@ -95,8 +98,10 @@ def getRowsLimit(table_name,no_row):
     
 def printRowsResult(rows):
     
+    count=0;
     for row in rows:
-        print "row-id:%s" %(row.row)
+        count = count + 1
+        print "row[%d]:%s" %(count,row.row)
         for k,v in sorted(row.columns.items()):
             print "%s:%s"%(k,v.value)
             
@@ -123,12 +128,25 @@ def main(args):
 #    getColumnInfo(table_name)            
 
     if(len(args)<2):
-        print "TableScan.py tableName"
+        print "TableScan.py tableName No[10]"
         sys.exit(1)
 
     table_name=args[1]
+    NO=10;
+    if(len(args)<3):
+        NO=10; 
+    else:
+        NO=int(args[2]);
 
-    ret=getRowsLimit(table_name,10)
+    getConfiguration('host.properties')
+
+    transport = TBufferedTransport(TSocket(hbaseHost, 9090))
+    transport.open()
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    global client
+    client = Hbase.Client(protocol)
+
+    ret=getRowsLimit(table_name,NO)
     printRowsResult(ret)
 
 
